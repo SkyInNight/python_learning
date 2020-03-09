d585fc783896b7f9a84b69a0ef5e4122cce75d51
NoBugNoLife <44198803+SkyInNight@users.noreply.github.com>
2020-03-09 23:23:00 +0800

    添加urllib设置headers参数和捕获异常代码。

---
+++ Python爬虫/src/crawler_base/four_set_headers_param.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+# 如果需要转为python2的话直接将as前面的代码块修改为import urllib2
+import urllib.request as req
+from urllib import error
+import random
+
+user_agent_pc_list = [
+    "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 "
+    "Safari/534.50",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; "
+    ".NET CLR 3.5.30729; InfoPath.3; rv:11.0) like Gecko",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",
+    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+    "Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11",
+    "Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 "
+    "Safari/535.11",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR "
+    "2.0.50727; SE 2.X MetaSr 1.0)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Avant Browser)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)"]
+
+user_agent_mobile_list = [
+    "Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 "
+    "Mobile/10A5376e Safari/8536.25",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0; HTC; Titan)",
+    "Mozilla/5.0 (SymbianOS/9.4; Series60/5.0 NokiaN97-1/20.0.019; Profile/MIDP-2.1 Configuration/CLDC-1.1) "
+    "AppleWebKit/525 (KHTML, like Gecko) BrowserNG/7.1.18124",
+    "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) "
+    "Version/5.0.2 Mobile/8J2 Safari/6533.18.5",
+    "Mozilla/5.0 (iPod; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) "
+    "Version/5.0.2 Mobile/8J2 Safari/6533.18.5",
+    "Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 "
+    "Mobile/8J2 Safari/6533.18.5",
+    "Mozilla/5.0 (Linux; U; Android 2.3.7; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) "
+    "Version/4.0 Mobile Safari/533.1",
+    "MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 ("
+    "KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+    "Opera/9.80 (Android 2.3.4; Linux; Opera Mobi/build-1107180945; U; en-GB) Presto/2.8.149 Version/11.10",
+    "Mozilla/5.0 (Linux; U; Android 3.0; en-us; Xoom Build/HRI39) AppleWebKit/534.13 (KHTML, like Gecko) Version/4.0 "
+    "Safari/534.13",
+    "Mozilla/5.0 (BlackBerry; U; BlackBerry 9800; en) AppleWebKit/534.1+ (KHTML, like Gecko) Version/6.0.0.337 Mobile "
+    "Safari/534.1+",
+    "Mozilla/5.0 (hp-tablet; Linux; hpwOS/3.0.0; U; en-US) AppleWebKit/534.6 (KHTML, like Gecko) wOSBrowser/233.70 "
+    "Safari/534.6 TouchPad/1.0"]
+
+
+def set_user_agent(os_type):
+    if os_type == "PC":
+        return user_agent_pc_list[random.randint(
+            0, len(user_agent_pc_list))]
+    elif os_type == "MOBILE":
+        return user_agent_mobile_list[random.randint(
+            0, len(user_agent_mobile_list))]
+    else:
+        return "Unknown Type！！"
+
+
+def download_test01(user_url):
+    """ 打印输出urllib爬虫返回对象参数内容
+
+    :param user_url: 网站url
+    """
+    response = req.urlopen(url=user_url, timeout=2)
+    print("返回对象类型为：{0}".format(type(response)))
+    print("返回请求信息为：\n{0}".format(response.info()))
+    print("网页前100的内容为：\n{0}".format(response.read(100)))
+
+
+def download_test02(user_url):
+    """ 添加爬虫异常获取
+
+    :param user_url: 网站url
+    """
+    try:
+        download_test01(user_url)
+    except error.URLError as e:
+        print("网络连接出错了，错误信息：{0}".format(e.reason))
+
+
+def download_add_pc_user_agent(user_url):
+    headers = {"User-Agent": set_user_agent("PC")}
+    request = req.Request(user_url, headers=headers)
+    try:
+        response = req.urlopen(request).read()
+        return response
+    except error.URLError as e:
+        print("网络连接出错了，错误信息：{0}".format(e.reason))
+
+
+if __name__ == '__main__':
+    print(set_user_agent("PC"))
+    url = r"http://www.baidu.com"
+    download_test01(url)
+    # 如果在代码执行的时候获取异常信息，可以避免代码在执行的时候被强制中断退出
+    url = r"http://www.google.com"
+    download_test02(url)
+    url = r"http://www.baidu.com"
+    print(download_add_pc_user_agent(url))
